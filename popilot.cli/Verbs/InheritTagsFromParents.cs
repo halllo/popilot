b77e5f25282677cb96feaeea870104039536bbf9
Manuel Naujoks <mnaujoks@gmail.com>
2025-07-24 19:12:55 +0200

    inherit tags from parents

---
+++ popilot.cli/Verbs/InheritTagsFromParents.cs
@@ -0,0 +1,48 @@
+﻿using CommandLine;
+using Microsoft.Extensions.Logging;
+using static ColoredConsole;
+
+namespace popilot.cli.Verbs
+{
+	[Verb("inherit-tags-from-parents")]
+	class InheritTagsFromParents
+	{
+		[Value(0, MetaName = "query ID (guid)", Required = true)]
+		public Guid QueryId { get; set; }
+
+		[Option(longName: "tags", HelpText = "Comma separated list of tags to inherit", Required = true)]
+		public string? RelevantTags { get; set; }
+
+		public async Task Do(AzureDevOps azureDevOps, ILogger<InheritTagsFromParents> logger)
+		{
+			var relevantTags = RelevantTags?.Split(",", StringSplitOptions.RemoveEmptyEntries) ?? [];
+			var items = await azureDevOps.GetQueryResultsFlat(QueryId);
+			logger.LogInformation($"Found {items.Count} items in query {QueryId}");
+			foreach (var item in items)
+			{
+				Display(item);
+				var chilRelevantTags = item.Tags.Intersect(relevantTags, InvariantCultureIgnoreCaseComparer.Instance).ToArray();
+				if (chilRelevantTags.Any())
+				{
+					logger.LogInformation("Already has relevant tag {@Tag}. Skipping.", chilRelevantTags);
+					continue;
+				}
+				else
+				{
+					var relevantParentTags = item.Parents.SelectMany(p => p.Tags).Intersect(relevantTags, InvariantCultureIgnoreCaseComparer.Instance).ToArray();
+					if (!relevantParentTags.Any())
+					{
+						logger.LogInformation("Parents don't have relevant tags to inherit. Skipping.");
+						continue;
+					}
+					else
+					{
+						var relevantTag = relevantParentTags.First();
+						logger.LogInformation("Inheriting tag {Tag} from parents {@Parents}...", relevantTag, item.Parents.Select(p => p.Id).ToArray());
+						await azureDevOps.AddTag(item.Id, relevantTag, CancellationToken.None);
+					}
+				}
+			}
+		}
+	}
+}
